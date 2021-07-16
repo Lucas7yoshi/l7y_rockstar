b6f9803307f2bf77f89f87e2f5bd60b39836aaeb
Lucas7yoshi <lucas7yoshi@gmail.com>
2021-07-16 08:28:00 -0400

    init

---
+++ client.lua
@@ -0,0 +1,29 @@
+RegisterCommand("rsstart", function()
+    if (IsRecording()) then
+        SendMsg("Already recording!")
+    else
+        SendMsg("Starting recording")
+        StartRecording(1)
+    end
+end)
+
+RegisterCommand("rsstop", function()
+    if (IsRecording()) then
+        StopRecordingAndSaveClip()
+    else
+        SendMsg("Wasn't recording!")
+    end    
+end)
+
+RegisterCommand("rsdiscard", function()
+    StopRecordingAndDiscardClip()
+    SendMsg("Discarded clip")
+end)
+
+RegisterCommand("rsopen", function()
+    ActivateRockstarEditor()
+end)
+
+function SendMsg(text)
+    TriggerEvent("chatMessage", "Rockstar Editor", {255, 255, 255}, text)
+end
